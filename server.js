b4555b7d547cce53cdc31b86e60ce7ae6d46f2fd
cyberxdevsofficial <cyberxdevsofficial@gmail.com>
2025-10-03 18:56:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/docs", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "docs.html"));
+});
+
+app.get("/marketplace", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "marketplace.html"));
 });
 
 app.listen(PORT, () => {
